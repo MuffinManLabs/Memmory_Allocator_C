c8180142c8ce93677462dc71597224b78dd0fb97
Hurayrah <hurayrah92@gmail.com>
2026-03-17 03:40:07 -0400

    working on the heap_init() memory slab allocater function

---
+++ allocator.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sys/mman.h>
+
+// void *ptr = mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+
+//call mmap once to get 4096-byte slab set up first
+//header at the start of the slab
+void * heap_init() {
+    void *slab_ptr = mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    return slab_ptr;
+} 
+
+//walk the linked list of headers
+//find first free block big enough (first-fit)
+//split the block if it's much bigger than needed
+//mark it as in use (free = 0)
+//return pointer to memory AFTER the header
+// my_malloc(size)
+
+
+//step back from ptr to find the header
+//mark it as free (free = 1)
+//coalesce with next block if it's also free
+// my_free(ptr)
+
+
+//walk the linked list
+//print each block's size, free/used status
+//(debugging tool to see the state of the slab)
+// heap_dump()
